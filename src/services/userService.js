b9fae9dd6b61825484f4dd0813cb396a4cebca22
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-07-06 14:59:01 +0600

    Create user_model

---
+++ src/services/userService.js
@@ -0,0 +1,37 @@
+const User = require("../models/user_models");
+const bcrypt = require("bcrypt");
+
+module.exports = {
+  async createUser(userData) {
+    try {
+      let { fullName, emailValue, password, role } = userData;
+      let existEmail = User.findOne({ email: emailValue });
+      if (existEmail) {
+        throw new Error("User Already Exist with Email");
+      }
+
+      password = await bcrypt.hash(password, 8);
+
+      const user = await User.create({
+        fullName,
+        email: emailValue,
+        password: password,
+        role,
+      });
+      return user;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  },
+
+  async getUserByEmail(email) {
+    try {
+      const user = User.findOne({ email: email });
+      if (!user) {
+        throw new Error("User Not Found");
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  },
+};
